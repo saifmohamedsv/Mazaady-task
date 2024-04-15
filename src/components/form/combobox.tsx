11a4e8fe41b3a3ebb6d3ff7a02a9e93741a1a6b1
saifmohamed-dev <155442820+saifmohamed-dev@users.noreply.github.com>
2024-04-15 15:19:41 +0200

    modify combobox to be re-usable

--- src/components/form/combobox.tsx
+++ src/components/form/combobox.tsx
@@ -18,30 +18,11 @@ import {
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
+interface ComboboxProps {
+  items: { value: string; label: string }[];
+}
 
-export function ComboboxDemo() {
+export function Combobox({ items }: ComboboxProps) {
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState("");
 
@@ -55,7 +36,7 @@ export function ComboboxDemo() {
           className="w-[200px] justify-between"
         >
           {value
-            ? frameworks.find((framework) => framework.value === value)?.label
+            ? items.find((item) => item.value === value)?.label
             : "Select framework..."}
           <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -65,20 +46,20 @@ export function ComboboxDemo() {
           <CommandInput placeholder="Search framework..." className="h-9" />
           <CommandEmpty>No framework found.</CommandEmpty>
           <CommandGroup>
-            {frameworks.map((framework) => (
+            {items.map((item) => (
               <CommandItem
-                key={framework.value}
-                value={framework.value}
+                key={item.value}
+                value={item.value}
                 onSelect={(currentValue) => {
                   setValue(currentValue === value ? "" : currentValue);
                   setOpen(false);
                 }}
               >
-                {framework.label}
+                {item.label}
                 <CheckIcon
                   className={cn(
                     "ml-auto h-4 w-4",
-                    value === framework.value ? "opacity-100" : "opacity-0"
+                    value === item.value ? "opacity-100" : "opacity-0"
                   )}
                 />
               </CommandItem>
