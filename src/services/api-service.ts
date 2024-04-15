3b12d97f9ac5c95c347301e396710da6487f54df
saifmohamed-dev <155442820+saifmohamed-dev@users.noreply.github.com>
2024-04-15 15:23:10 +0200

    adding api service

---
+++ src/services/api-service.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosInstance } from "axios";
+
+const baseURL = process.env.API_BASE_URL;
+const apiKey = process.env.PRIVATE_KEY;
+
+const api: AxiosInstance = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+  },
+});
+
+export default api;
