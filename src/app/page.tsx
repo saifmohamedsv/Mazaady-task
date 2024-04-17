4225f3ffe79bd303851025ade78ffd91d87e1ca8
saifmohamed-dev <155442820+saifmohamed-dev@users.noreply.github.com>
2024-04-17 12:19:58 +0200

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
 import { Categories } from "@/components/common";
 import { SubCategories } from "@/components/common/subcategories-combobox";
-import { Button } from "@/components/ui";
-import { api } from "@/services";
+import { api, getCategories } from "@/services";
 import { Category } from "@/types";
 import clsx from "clsx";
-import { useState } from "react";
 import { Properties, StorePrettyDisplay } from "./components";
 import { ShowValuesTable } from "./components/show-values-table";
-import { ValuesTable } from "./components/values-table";
 
-async function getCategories(): Promise<Category[]> {
-  const res = await api.get("/get_all_cats");
 
-  if (res.status !== 200) {
-    throw new Error("Failed to fetch categories");
-  }
-
-  return res.data.data?.categories;
-}
 
 export default async function Home() {
   const categories = await getCategories();
--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -1,7 +1,11 @@
+import { Category } from "@/types";
 import axios, { AxiosInstance } from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const apiKey = process.env.NEXT_PUBLIC_PRIVATE_KEY;
+const baseURL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://staging.mazaady.com/api/v1/";
+const apiKey =
+  process.env.NEXT_PUBLIC_PRIVATE_KEY || "3%o8i}_;3D4bF]G5@22r2)Et1&mLJ4?$@+16";
+// I did this bad practice because of the time, while deploying I figured out it's failing while building
 
 export const api: AxiosInstance = axios.create({
   baseURL,
@@ -18,4 +22,14 @@ export async function getProperties(categoryId: string) {
   }
 
   return res.data;
+}
+
+export async function getCategories(): Promise<Category[]> {
+  const res = await api.get("/get_all_cats");
+
+  if (res.status !== 200) {
+    throw new Error("Failed to fetch categories");
+  }
+
+  return res.data.data?.categories;
 }
